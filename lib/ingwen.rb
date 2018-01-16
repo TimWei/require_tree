adc987ba15a3c68831cccdb877342b22c7f15969
tim <tim.w@axsoftware.net>
2018-01-16 20:18:26 +0800

    add recursive

--- lib/ingwen.rb
+++ lib/ingwen.rb
@@ -1,11 +1,34 @@
 module Kernel
-  def require_tree path,base=Dir.pwd
-    Dir["#{base}/#{path}/*"].each {|file| 
-      if File.directory? file
-        require_tree '.', file
-      elsif file =~ /\.rb$/
-        require_relative file 
+  def require_tree path
+    rb_files_queue = []  
+    parse_path path, Dir.pwd, rb_files_queue
+    try_and_error rb_files_queue
+  end
+
+  private
+  def parse_path path, base, queue
+    Dir["#{base}/#{path}/*"].each {|path| 
+      begin
+        if File.directory? path
+          parse_path '.', path, queue
+        elsif path =~ /\.rb$/
+          queue << path 
+        end
       end
     }
+  end
+
+  def try_and_error queue
+    max_count = queue.count
+    loaded = []
+    queue.each_with_index do |file, i|
+      begin
+        require file
+        loaded << file
+      rescue Exception => e   
+        next
+      end
+    end 
+    try_and_error(queue - loaded) if loaded.count > 0
   end
 end
